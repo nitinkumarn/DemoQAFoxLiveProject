76d6ba61a08925f016c6ef68eb0a2f2766b8b249
Nitin Kumar N <nitin@Rahuls-MacBook-Air.local>
2021-07-13 19:10:43 +0530

    updated the framework and started creating automation scripts for register functionality

---
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -0,0 +1,41 @@
+package com.tutorialsninja.automation.stepdef;
+
+import com.tutorialsninja.automation.base.Base;
+
+import cucumber.api.java.en.*;
+
+public class Register{
+	
+	
+	@Given("^I launch the application$")
+	public void i_launch_the_application(){
+	    Base.driver.get(Base.reader.getUrl());
+	    
+	}
+
+	@And("^I navigate to Account registeration page$")
+	public void i_navigate_to_Account_registeration_page(){
+	    
+	}
+
+	@When("^I provide all the below details$")
+	public void i_provide_all_the_below_details(){
+	    
+	}
+
+	@And("^I select the privacy policy$")
+	public void i_select_the_privacy_policy(){
+	    
+	}
+
+	@And("^I click on continue button$")
+	public void i_click_on_continue_button(){
+	    
+	}
+
+	@Then("^I should see that the user account has been created successfully$")
+	public void i_should_see_that_the_user_account_has_been_created_successfully(){
+	    
+	}
+
+}
---
+++ src/test/resources/FeatureFiles/Register.feature
@@ -0,0 +1,15 @@
+Feature: Registration functionality scenarioes
+
+@Register @one
+Scenario: Verify whether user is able to register into the application by providing all the details
+	Given I launch the application
+	And I navigate to Account registeration page
+	When I provide all the below details
+	| Firstname | Nitin 										|
+	| Last name | Kumar										  |
+	| Email     | niti.1si12me093@gmail.com |
+	| Telephone | 7204940016 								|
+	| Password  | 123456 										|
+	And I select the privacy policy
+	And I click on continue button
+	Then I should see that the user account has been created successfully
